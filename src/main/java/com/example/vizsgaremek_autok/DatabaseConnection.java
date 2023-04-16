b1cc1189d4051b35d3f32863bab4c25746d2ef20
LeviVitez <bmwyolo555@gmail.com>
2023-04-17 01:38:42 +0200

    login auth working

--- src/main/java/com/example/vizsgaremek_autok/DatabaseConnection.java
+++ src/main/java/com/example/vizsgaremek_autok/DatabaseConnection.java
@@ -7,21 +7,10 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     public Connection databaseLink;
 
-    public static String DB_DRIVER = "mysql";
-    public static String DB_HOST = "localhost";
-    public static String DB_PORT = "3306";
-    public static String DB_DBNAME = "vizsgaremek";
-    public static String DB_USER = "root";
-    public static String DB_PASS = "";
-
-    public DatabaseConnection() throws SQLException {
-        String url = String.format("jdbc:%s://%s:%s/%s", DB_DRIVER, DB_HOST, DB_PORT, DB_DBNAME);
-        databaseLink = DriverManager.getConnection(url, DB_USER, DB_PASS);
-    }
 
     public Connection getConnection(){
         String databaseName="vizsgaremek";
-        String databaseUser="de";
+        String databaseUser="root";
         String databasePassword="";
         String url="jdbc:mysql://localhost/" + databaseName;
 
