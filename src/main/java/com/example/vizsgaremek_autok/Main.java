fca0ba4ddaa85cc3c1611040829c69e50760b3d7
LeviVitez <bmwyolo555@gmail.com>
2023-04-16 21:31:01 +0200

    essentials

--- src/main/java/com/example/vizsgaremek_autok/Main.java
+++ src/main/java/com/example/vizsgaremek_autok/Main.java
@@ -7,11 +7,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 520, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
