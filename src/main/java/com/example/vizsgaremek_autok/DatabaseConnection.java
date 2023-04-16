95dfd1d2319d929527fab7cbf9830f1ec96f296c
LeviVitez <bmwyolo555@gmail.com>
2023-04-17 01:19:40 +0200

    working on login

--- src/main/java/com/example/vizsgaremek_autok/DatabaseConnection.java
+++ src/main/java/com/example/vizsgaremek_autok/DatabaseConnection.java
@@ -2,9 +2,22 @@ package com.example.vizsgaremek_autok;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseConnection {
     public Connection databaseLink;
+
+    public static String DB_DRIVER = "mysql";
+    public static String DB_HOST = "localhost";
+    public static String DB_PORT = "3306";
+    public static String DB_DBNAME = "vizsgaremek";
+    public static String DB_USER = "root";
+    public static String DB_PASS = "";
+
+    public DatabaseConnection() throws SQLException {
+        String url = String.format("jdbc:%s://%s:%s/%s", DB_DRIVER, DB_HOST, DB_PORT, DB_DBNAME);
+        databaseLink = DriverManager.getConnection(url, DB_USER, DB_PASS);
+    }
 
     public Connection getConnection(){
         String databaseName="vizsgaremek";
