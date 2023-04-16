736f2adaf242cc57d68f01095c4d8cd28ec93526
LeviVitez <bmwyolo555@gmail.com>
2023-04-16 22:44:52 +0200

    styling

--- src/main/java/com/example/vizsgaremek_autok/Main.java
+++ src/main/java/com/example/vizsgaremek_autok/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -12,7 +13,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 520, 400);
-        stage.setTitle("Hello!");
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
         stage.show();
     }
