2184432bb6b0c8bb33577cbf39f1789ef3106fed
Boton <botond.pfeffer@gmail.com>
2023-04-17 16:08:14 +0200

    fxml fields invited

--- src/main/java/com/example/vizsgaremek_autok/CarDataController.java
+++ src/main/java/com/example/vizsgaremek_autok/CarDataController.java
@@ -1,4 +1,38 @@
 package com.example.vizsgaremek_autok;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class CarDataController {
+    @FXML
+    private TextField carNameLabel;
+    @FXML
+    private TextField CarBrandLabel;
+    @FXML
+    private TextField ModelLabel;
+    @FXML
+    private TextField ModelYearLabel;
+    @FXML
+    private TextField FuelLabel;
+    @FXML
+    private TextField CarPowerLabel;
+    @FXML
+    private TextField GearTypeLabel;
+    @FXML
+    private TextField ColorLabel;
+    @FXML
+    private TextField ChassiTypeLabel;
+    @FXML
+    private TextField DoorsLabel;
+    @FXML
+    private TextField FuelEconomyLabel;
+    @FXML
+    private TextField LicencePlateLabel;
+    @FXML
+    private Button UploadBut;
+    @FXML
+    private Button ExitBut;
+
+
 }
